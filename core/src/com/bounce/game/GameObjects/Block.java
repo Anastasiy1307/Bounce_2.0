2665c38684d1e52021c50bd30466ab4af7124d8c
Anastasiy1307 <100847962+Anastasiy1307@users.noreply.github.com>
2022-11-25 21:59:00 +0300

    Texture

---
+++ core/src/com/bounce/game/GameObjects/Block.java
@@ -0,0 +1,15 @@
+package com.bounce.game.GameObjects;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Block extends MainObject{
+
+    public Block(float x, float y, float width, float height, String texture){
+        this.texture = new com.badlogic.gdx.graphics.Texture(texture);
+        this.texture.setFilter(com.badlogic.gdx.graphics.Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        rectangle = new Rectangle(x, y, width, height);
+
+    }
+
+}
---
+++ core/src/com/bounce/game/GameObjects/Spike.java
@@ -0,0 +1,14 @@
+package com.bounce.game.GameObjects;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Spike/*Шип*/ extends MainObject{
+
+    public Spike(float x, float y, float width, float height, String texture){
+        this.texture = new com.badlogic.gdx.graphics.Texture(texture);
+        this.texture.setFilter(com.badlogic.gdx.graphics.Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        rectangle = new Rectangle(x, y, width, height);
+
+    }
+}
